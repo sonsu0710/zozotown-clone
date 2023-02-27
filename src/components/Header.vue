607fc23f0a923fc66b80039bfd5559ec27e0d38d
sonsu0710 <sonsu@tagby.io>
2023-02-28 00:41:19 +0900

    Header 검색창 제작중

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,31 @@
   <header class="header">
     <div class="header--container">
       <a class="header--logo">ZOZOTOWN</a>
-      <form class="header--input">
-        <label class="header--input__label">
-          <img class="header--input__search" src="@/img/search.svg"/>
-          <input class="header--input__box" placeholder="すべてのアイテムから探す"/>
-        </label>
-      </form>
+      <div class="header--input" >
+        <form>
+          <label class="header--input__label">
+            <img class="header--input__search" src="@/img/search.svg"/>
+            <input
+                class="header--input__box"
+                autocomplete="off"
+                placeholder="すべてのアイテムから探す"
+                name="header-search"
+                @input="inputValue = $event.target.value"
+            />
+          </label>
+          <div v-if="inputValue" class="header--search-box">
+            <div
+                class="header--search-box__result"
+                v-for="(item, idx) in resultValue"
+                :key="item.value"
+                :class="(idx === 9 ? searchModalLast : '') || (idx === 0 ? searchModalFirst : '')"
+            >
+              {{item.value}}
+              {{idx}}
+            </div>
+          </div>
+        </form>
+      </div>
       <div class="header--gnb">
         <a class="header--gnb__login">
           <div>ログイン</div>
@@ -30,8 +49,45 @@
 </template>
 
 <script lang="ts" setup>
+import {computed, ref, watch} from "vue";
+import axios from "axios";
+
 // Todo => 검색창을 선택할경우 포커스되며 배경이 투명하게 변함.
+
+
 // Todo => 검색창에 검색어가 입력될 경우 오른쪽 끝에 X표시. X 표시를 누르면 검색어 리셋
+
+
+// Todo => 검색어 입력시 예상 검색어 모달창 나오도록 표시
+const inputValue = ref("");
+const resultValue = ref([]);
+
+const getSearchData = async () => {
+  await axios
+      .get(`https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${inputValue.value}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`)
+      .then((res) => {
+        resultValue.value = res.data.suggestions;
+        console.log(resultValue.value);
+      })
+};
+watch(inputValue, () => {
+  getSearchData();
+})
+
+const searchInputBottomRadius = computed(() => {
+  return "header--input__label-bottom"
+})
+
+
+const searchModalFirst = computed(() => {
+  return "header--search-box__result-first";
+})
+const searchModalLast = computed(() => {
+  return "header--search-box__result-last";
+})
+
+// Todo 검색창에서 자동완성되는 경우 해당 인풋 백그라운드 색상 제거
+
 </script>
 
 <style lang="scss" scoped>
