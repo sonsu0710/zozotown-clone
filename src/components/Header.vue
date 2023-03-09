3a7581885e556621d39f3d8e80291a96299817e0
Sonsu <sonsu0710@gmail.com>
2023-03-09 22:08:46 +0900

    input 태그 폰트 굵기 변경 및 미세한 디자인 조정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,13 @@
   <header class="header">
     <div class="header--container">
       <a class="header--logo">ZOZOTOWN</a>
-      <div class="header--input" >
+      <div class="header--input">
         <form>
-          <label class="header--input__label">
-            <img class="header--input__search" src="@/img/search.svg"/>
+          <label
+              class="header--input__label"
+              :class="inputValue ? borderBottomRadius : ''"
+          >
+            <img class="header--input__search" src="@/img/search.svg" alt="search"/>
             <input
                 class="header--input__box"
                 autocomplete="off"
@@ -22,7 +25,6 @@
                 :class="(idx === 9 ? searchModalLast : '') || (idx === 0 ? searchModalFirst : '')"
             >
               {{item.value}}
-              {{idx}}
             </div>
           </div>
         </form>
@@ -52,13 +54,13 @@
 import {computed, ref, watch} from "vue";
 import axios from "axios";
 
-// Todo => 검색창을 선택할경우 포커스되며 배경이 투명하게 변함.
+// todo => 검색창을 선택할경우 포커스되며 배경이 투명하게 변함.
 
 
-// Todo => 검색창에 검색어가 입력될 경우 오른쪽 끝에 X표시. X 표시를 누르면 검색어 리셋
+// todo => 검색창에 검색어가 입력될 경우 오른쪽 끝에 X표시. X 표시를 누르면 검색어 리셋
 
 
-// Todo => 검색어 입력시 예상 검색어 모달창 나오도록 표시
+// todo => 검색어 입력시 예상 검색어 모달창 나오도록 표시
 const inputValue = ref("");
 const resultValue = ref([]);
 
@@ -67,17 +69,15 @@ const getSearchData = async () => {
       .get(`https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${inputValue.value}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`)
       .then((res) => {
         resultValue.value = res.data.suggestions;
-        console.log(resultValue.value);
       })
 };
 watch(inputValue, () => {
   getSearchData();
 })
 
-const searchInputBottomRadius = computed(() => {
+const borderBottomRadius = computed(() => {
   return "header--input__label-bottom"
 })
-
 
 const searchModalFirst = computed(() => {
   return "header--search-box__result-first";
@@ -86,7 +86,7 @@ const searchModalLast = computed(() => {
   return "header--search-box__result-last";
 })
 
-// Todo 검색창에서 자동완성되는 경우 해당 인풋 백그라운드 색상 제거
+// todo 검색창에서 자동완성되는 경우 해당 인풋 백그라운드 색상 제거
 
 </script>
 
